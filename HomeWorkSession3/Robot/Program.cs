a60b47dac06e4580bfa360647ba51b60f011efae
ionel moreanu <47196725+imoreanu@users.noreply.github.com>
2023-05-08 18:46:55 +0300

    HomeWorkSession3

---
+++ HomeWorkSession3/Robot/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Automaton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Robot robot = new Robot("Wall-E");
+            robot.DisplayName();
+            robot.RobotActions();
+        }
+    }
+}
+
---
+++ HomeWorkSession3/Robot/Robot.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+
+namespace Automaton
+{
+    public class Robot
+    {
+        public string Name { get; set; }
+        public Robot(string robotname) 
+        {
+            Name = robotname;
+        }
+        public void DisplayName()
+        {
+            Console.WriteLine($"Hi, my name is {Name}");
+        }
+        public void RobotActions()
+        {
+            var keypress = "";
+            Console.WriteLine("Enter any Key: ");
+            keypress = Console.ReadLine();
+            switch (keypress)
+            {
+                case "5":
+                    HighFiveAction();
+                    break;
+                default:
+                    Console.WriteLine("This is not the way to do it");
+                    break;
+            }
+        }
+        public void HighFiveAction()
+        {
+            Console.WriteLine("Wall-E high 5'ed you!");
+        }
+    }
+}
